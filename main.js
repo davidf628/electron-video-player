9005ad745e99377fcb269fb7d2a48c7363778a5b
davidf628 <30780186+davidf628@users.noreply.github.com>
2023-10-06 13:02:35 -0400

    Completed reading/writing to disk
    Created the logic to read and write the data to disk using compression

--- main.js
+++ main.js
@@ -3,17 +3,20 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 const path = require('node:path')
-const fs = require('fs');
-const fsPromises = require('fs').promises;
+const fs = require('node:fs');
+const fsPromises = require('node:fs').promises;
+const os = require('node:os');
+const crypto = require('node:crypto');
+const zlib = require('node:zlib');
 const misc = require('./js/misc.js');
 
-const version = "0.4.3";
+const version = "0.6.0";
 
 const createWindow = () => {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
         width: 800,
-        height: 600,
+        height: 650,
         webPreferences: {
         preload: path.join(__dirname, 'preload.js')
         }
@@ -23,50 +26,38 @@ const createWindow = () => {
     mainWindow.loadFile('index.html');
     console.log(app.getPath('userData'));
 
-    ipcMain.on('save-video-data', (event, data) => {
-        console.log(`VIDEO ID == ${data.video_id}`);
-        let payload = `\n${JSON.stringify(data)}`
-        fs.appendFile('./output.data', payload, (err) => {
-            if (err) {
-                console.error(`An error occured while writing to the file ...`);
-            } else {
-                console.log(`... was written successfully!`);
-            }
-        });
+};
 
-});
+//   ipcMain.on('create-new-video-database', async (event) => {
+//       const window = BrowserWindow.getFocusedWindow();
+//       const options = {
+//           title: 'Create a New File',
+//           properties: ['openFile']
+//       };
+//       const result = await dialog.showOpenDialog(window, options);
+//       if (result.filePaths && result.filePaths.length > 0) {
+//           window.webContents.send('editor-event', { 
+//               action: 'set-directory', 
+//               data: result.filePaths[0]
+//           })
+//       }
+//   });
 
-  ipcMain.on('create-new-video-database', async (event) => {
-      const window = BrowserWindow.getFocusedWindow();
-      const options = {
-          title: 'Create a New File',
-          properties: ['openFile']
-      };
-      const result = await dialog.showOpenDialog(window, options);
-      if (result.filePaths && result.filePaths.length > 0) {
-          window.webContents.send('editor-event', { 
-              action: 'set-directory', 
-              data: result.filePaths[0]
-          })
-      }
-  });
+//   ipcMain.on('open-video-database', async (event) => {
+//     const window = BrowserWindow.getFocusedWindow();
+//     const options = {
+//         title: 'Open a Video DataFile',
+//         properties: ['openDirectory']
+//     };
+//     const result = await dialog.showOpenDialog(window, options);
+//     if (result.filePaths && result.filePaths.length > 0) {
+//         window.webContents.send('editor-event', { 
+//             action: 'open-file', 
+//             data: result.filePaths[0]
+//         })
+//     }
+//   });
 
-  ipcMain.on('open-video-database', async (event) => {
-    const window = BrowserWindow.getFocusedWindow();
-    const options = {
-        title: 'Open a Video DataFile',
-        properties: ['openDirectory']
-    };
-    const result = await dialog.showOpenDialog(window, options);
-    if (result.filePaths && result.filePaths.length > 0) {
-        window.webContents.send('editor-event', { 
-            action: 'open-file', 
-            data: result.filePaths[0]
-        })
-    }
-  });
-
-}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -76,6 +67,7 @@ app.whenReady().then(() => {
     ipcMain.handle('get-video-data', fetch_video_data);
     ipcMain.handle('get-intervals-watched', get_intervals_watched);
     ipcMain.handle('get-version', get_version_number);
+    ipcMain.on('save-video-data', save_progress_to_disk);
 
     createWindow();
 
@@ -101,9 +93,13 @@ app.on('window-all-closed', () => {
  *  the math 104 youtube vidoes my students can be tracked on
  */
 async function fetch_video_data() {
-    let data = await fetch('https://davidf628.github.io/video_data_104.json')
-    let payload = await data.json();
-    return payload;
+    try {
+        let data = await fetch('https://davidf628.github.io/video_data_104.json')
+        let payload = await data.json();
+        return payload;
+    } catch(error) {
+        console.log('There was an error accessing video database.')
+    }
 }
 
 /******************************************************************************
@@ -112,22 +108,23 @@ async function fetch_video_data() {
  */
 async function get_intervals_watched(event, video) {
 
-    // read the data file - will need to change this to student selected file
-    const data = await fsPromises.readFile('./output.data')
-        .catch((err) => console.error('Failed to read file', err));
-
-    // convert the raw data into lines of text
-    let lines = data.toString().split('\n');
     let view_data = [];
 
-    // go through the lines and pick the ones that match the given video_id
-    for (let line of lines) {
-        let obj = JSON.parse(line);
-        //console.log(obj);
-        //console.log(`looking for == ${JSON.stringify(video)}`);
-        if (video === obj.video_id) {
-            view_data = misc.union_intervals(obj.intervals_watched, view_data);
-            //console.log(obj.intervals_watched);
+    // read the data file - will need to change this to student selected file
+    let data = read_data_from_disk('./output.data')
+
+    console.log(`DATA == ${data}`);
+    
+    if (data.trim().length > 0) {
+        let lines = data.split('\n');
+        // go through the lines and pick the ones that match the given video_id
+        for (let line of lines) {
+            if (line.trim().length > 0) {
+                let obj = JSON.parse(line);
+                if (video === obj.video_id) {
+                    view_data = misc.union_intervals(obj.intervals_watched, view_data);
+                }
+            }
         }
     }
 
@@ -137,19 +134,77 @@ async function get_intervals_watched(event, video) {
 
 
 /******************************************************************************
+ * Saves user progress to disk
+ */
+
+function save_progress_to_disk (event, data) {
+
+    let mainWindow = BrowserWindow.getFocusedWindow();
+
+    data.username = os.userInfo().username;
+    data.fingerprint = crypto.createHash('md5').update(`${data.timestamp}${data.student_id}`).digest('hex');
+    let payload = `${JSON.stringify(data)}\n`;
+
+    // Combine the new data with the data already saved on disk
+    let current_data = read_data_from_disk('./output.data');
+
+    let buffer = `${current_data}${payload}`;
+
+    // Zip the data up to save space and encode it in base64
+    buffer = encode_data(buffer);
+
+    try {
+        fs.writeFileSync('./output.data', buffer);
+        dialog.showMessageBoxSync(mainWindow, { message: "File saved." });
+    } catch (error) {
+        dialog.showErrorBox("Error writing file", "An error occurred while attempting to save data.");
+    }
+}
+
+function read_data_from_disk(filename) {
+
+    // Read data from disk which is in base64 encoding
+    let data = fs.readFileSync(filename).toString();
+    data = decode_data(data);
+
+    return data;
+}
+
+/******************************************************************************
+ * Decodes data written to disk. The buffer is assumed to be a base64 string 
+ *  and contains a zlib zip encoding scheme
+ */
+function decode_data(buffer) {
+    let data_uncompressed = '';
+
+    if (buffer.trim().length > 0) {
+        // Decode the base64 string to binary
+        let data_compressed = new Buffer.from(buffer, 'base64');
+
+        // Unzip the binary string
+        data_uncompressed = zlib.inflateSync(data_compressed).toString();
+    }
+    return data_uncompressed;
+}
+
+/******************************************************************************
+ * Encodes data so that it is ready to be written to disk. It first uses zlib
+ *  to compress the data into a smaller chunk and then converts it into 
+ *  base64.
+ */
+
+function encode_data(buffer) {
+
+    let compressed_data = zlib.deflateSync(buffer);
+    let encoded_data = new Buffer.from(compressed_data).toString('base64');
+
+    // Zip the data up to save space and encode it in base64
+    return encoded_data;
+}
+
+/******************************************************************************
  * Get the current version number and passes it over to the main program
  */
 async function get_version_number () {
     return version;
-}
-
-/******************************************************************************
- * Combines two sets of intervals using the set operation of union to make
- *  sure no time ranges are duplicated
- */
-
-function union_intervals (interval1, interval2) {
-
-    
-
 }
--- test.js
+++ test.js
@@ -1,59 +1,105 @@
-const misc = require('./js/misc.js');
+const fs = require('node:fs');
+const zlib = require('node:zlib');
 
-let int = [ [0, 65], [250, 310] ];
+function decode_data(buffer) {
+    let data_uncompressed = '';
 
-// case 1: u is completely outside all intervals
-let case1 = [ [75, 150] ];
-let expected1 = [ [0, 65], [75, 150], [ 250, 310] ];
-console.log(`\nTest 1: u is completely outside all intervals\n\n
-            ${JSON.stringify(case1)} U ${JSON.stringify(int)} = 
-            ${JSON.stringify(misc.union_intervals(int, case1))}\n
-            Expected: ${JSON.stringify(expected1)}`);
+    if (buffer.trim().length > 0) {
+        // Decode the base64 string to binary
+        let data_compressed = new Buffer.from(buffer, 'base64');
 
-// case 2: u is completely contained within an interval
-let case2 = [ [275, 300] ];
-let expected2 = [ [0, 65], [250, 310] ];
-console.log(`\nTest 2: u is completely contained within an interval\n\n
-            ${JSON.stringify(case2)} U ${JSON.stringify(int)} = 
-            ${JSON.stringify(misc.union_intervals(int, case2))}\n
-            Expected: ${JSON.stringify(expected2)}`);
+        // Unzip the binary string
+        data_uncompressed = zlib.inflateSync(data_compressed).toString();
+    }
+    return data_uncompressed;
+}
 
-// case 3: u completely contains an interval
-let case3 = [ [100, 400] ];
-let expected3 = [ [0, 65], [100, 400] ];
-console.log(`\nTest 3: u completely contains an interval\n\n
-            ${JSON.stringify(case3)} U ${JSON.stringify(int)} = 
-            ${JSON.stringify(misc.union_intervals(int, case3))}\n
-            Expected: ${JSON.stringify(expected3)}`);
+function encode_data(buffer) {
 
-// case 4: u contains a lower bound
-let case4 = [ [100, 300] ];
-let expected4 = [ [0, 65], [100, 310] ];
-console.log(`\nTest 4: u contains a lower bound\n\n
-            ${JSON.stringify(case4)} U ${JSON.stringify(int)} = 
-            ${JSON.stringify(misc.union_intervals(int, case4))}\n
-            Expected: ${JSON.stringify(expected4)}`);
+    let compressed_data = zlib.deflateSync(buffer);
 
-// case 5: u contains an upper bound
-let case5 = [ [300, 400] ];
-let expected5 = [ [0, 65], [250, 400] ];
-console.log(`\nTest 5: u contains a upper bound\n\n
-            ${JSON.stringify(case5)} U ${JSON.stringify(int)} = 
-            ${JSON.stringify(misc.union_intervals(int, case5))}\n
-            Expected: ${JSON.stringify(expected5)}`);
+    let encoded_data = new Buffer.from(compressed_data).toString('base64');
 
-// case 6: u spans the start and end of two intervals
-let case6 = [ [50, 300] ];
-let expected6 = [ [0, 310] ];
-console.log(`\nTest 6: u spans the start and end of two intervals\n\n
-            ${JSON.stringify(case6)} U ${JSON.stringify(int)} = 
-            ${JSON.stringify(misc.union_intervals(int, case6))}\n
-            Expected: ${JSON.stringify(expected6)}`);
+    // Zip the data up to save space and encode it in base64
+    return encoded_data;
+}
 
-//let int = [ [0, 65], [250, 310] ];
-let test = [ [10, 25], [40, 70], [72, 90], [100, 200], [300, 500], [700, 800] ];
-let expected = [ [0, 90], [100, 200], [250, 500], [700, 800] ];
-console.log(`\nTest 7: combining multiple intervals\n\n
-            ${JSON.stringify(test)} U ${JSON.stringify(int)} = 
-            ${JSON.stringify(misc.union_intervals(int, test))}\n
-            Expected: ${JSON.stringify(expected)}`);
+// let data = '12345';
+// console.log(`DATA == ${data}\n\n`);
+// let encode = encode_data(data);
+// console.log(`ENCODED == ${encode}\n\n`);
+// let decode = decode_data(encode);
+// console.log(`DECODED == ${decode}`);
+
+let data = fs.readFileSync('./output.data').toString();
+console.log(`\n\nDATA == \n\n\n${data}`);
+
+let decoded = decode_data(data);
+console.log(`\n\nDECODED == \n\n${decoded}`);
+
+// data = atob(data);
+// console.log(`\n\nBUFFER == \n\n\n${data}`);
+
+// data = zlib.inflateSync(data);
+
+// console.log(`\n\nDECODE == \n\n\n${data}`);
+
+//const misc = require('./js/misc.js');
+
+// let int = [ [0, 65], [250, 310] ];
+
+// // case 1: u is completely outside all intervals
+// let case1 = [ [75, 150] ];
+// let expected1 = [ [0, 65], [75, 150], [ 250, 310] ];
+// console.log(`\nTest 1: u is completely outside all intervals\n\n
+//             ${JSON.stringify(case1)} U ${JSON.stringify(int)} = 
+//             ${JSON.stringify(misc.union_intervals(int, case1))}\n
+//             Expected: ${JSON.stringify(expected1)}`);
+
+// // case 2: u is completely contained within an interval
+// let case2 = [ [275, 300] ];
+// let expected2 = [ [0, 65], [250, 310] ];
+// console.log(`\nTest 2: u is completely contained within an interval\n\n
+//             ${JSON.stringify(case2)} U ${JSON.stringify(int)} = 
+//             ${JSON.stringify(misc.union_intervals(int, case2))}\n
+//             Expected: ${JSON.stringify(expected2)}`);
+
+// // case 3: u completely contains an interval
+// let case3 = [ [100, 400] ];
+// let expected3 = [ [0, 65], [100, 400] ];
+// console.log(`\nTest 3: u completely contains an interval\n\n
+//             ${JSON.stringify(case3)} U ${JSON.stringify(int)} = 
+//             ${JSON.stringify(misc.union_intervals(int, case3))}\n
+//             Expected: ${JSON.stringify(expected3)}`);
+
+// // case 4: u contains a lower bound
+// let case4 = [ [100, 300] ];
+// let expected4 = [ [0, 65], [100, 310] ];
+// console.log(`\nTest 4: u contains a lower bound\n\n
+//             ${JSON.stringify(case4)} U ${JSON.stringify(int)} = 
+//             ${JSON.stringify(misc.union_intervals(int, case4))}\n
+//             Expected: ${JSON.stringify(expected4)}`);
+
+// // case 5: u contains an upper bound
+// let case5 = [ [300, 400] ];
+// let expected5 = [ [0, 65], [250, 400] ];
+// console.log(`\nTest 5: u contains a upper bound\n\n
+//             ${JSON.stringify(case5)} U ${JSON.stringify(int)} = 
+//             ${JSON.stringify(misc.union_intervals(int, case5))}\n
+//             Expected: ${JSON.stringify(expected5)}`);
+
+// // case 6: u spans the start and end of two intervals
+// let case6 = [ [50, 300] ];
+// let expected6 = [ [0, 310] ];
+// console.log(`\nTest 6: u spans the start and end of two intervals\n\n
+//             ${JSON.stringify(case6)} U ${JSON.stringify(int)} = 
+//             ${JSON.stringify(misc.union_intervals(int, case6))}\n
+//             Expected: ${JSON.stringify(expected6)}`);
+
+// //let int = [ [0, 65], [250, 310] ];
+// let test = [ [10, 25], [40, 70], [72, 90], [100, 200], [300, 500], [700, 800] ];
+// let expected = [ [0, 90], [100, 200], [250, 500], [700, 800] ];
+// console.log(`\nTest 7: combining multiple intervals\n\n
+//             ${JSON.stringify(test)} U ${JSON.stringify(int)} = 
+//             ${JSON.stringify(misc.union_intervals(int, test))}\n
+//             Expected: ${JSON.stringify(expected)}`);
